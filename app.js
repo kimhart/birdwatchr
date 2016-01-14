233b3e174e4b55e1683aa515c72728755224231a
Kim Hart <kimhart90@gmail.com>
2016-01-14 09:14:05 -0500

    bird homework

--- app.js
+++ app.js
@@ -1,15 +1,76 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var handlebars = require('handlebars');
+var geocoder = require('geocoder');
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/birds';
+var ObjectId = require('mongodb').ObjectId;
 
-// Configuration
+var db;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-// Routes
+MongoClient.connect(mongoUrl, function(err, database){
+  if(err){
+    console.log(err);
+  }
+  db = database;
+  process.on("exit", db.close); 
+});
+
+
 app.get('/', function(req, res){
   res.render('index');
 });
 
+app.get('/sightings', function(req, res){
+  db.collection('sightings').find({}).limit(3).sort({date: -1}).toArray(function(err, result){
+      result.date = Date();
+    res.json(result);
+    });
+});
+
+app.get('/sightings/new', function(req, res){
+  res.render ('form');
+});
+
+app.post('/sightings', function(req, res){
+  geocoder.geocode(req.body.location, function ( err, result ){
+    var latLong = result.results[0].geometry.location;
+    db.collection('sightings').insert({
+      date: Date (),
+      species: req.body.species,
+      gender: req.body.gender,
+      location: req.body.location,
+      lat_long: latLong
+    },function(err, result){
+    });
+  });
+  res.redirect('/');
+});
+
+app.get('/demo', function(req, res){
+  res.render('demo');
+});
+
+app.get('/demo/sightings', function(req, res){
+  db.collection('sightings').find({}).sort({date: -1}).toArray(function(err, result){
+    res.json(result);
+  });
+});
+
+app.post('/demo', function(req, res){
+  geocoder.geocode(req.body.location, function ( err, result ){
+    var latLong = result.results[0].geometry.location;
+    console.log(latLong);
+  },function(err, result){
+  });
+  res.end();
+});
+
+
 app.listen(process.env.PORT || 3000);
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,36 @@
-console.log("         _     ")
-console.log("        <')_,/ ")
-console.log("        (_==/  ")
-console.log("birder  ='-    ")
-console.log("we're hiring!  ")
+var getSightings = function(){
+  $.ajax({
+      url:"/sightings",
+      type: "GET",
+      dataType: "json"
+    }).done(function(response){  
+      var source = $("#sighting-info").html();
+      var template = Handlebars.compile(source);
+      response.forEach(function(sighting){
+        $('body').append(template(sighting));
+      });
+    });
+};
+
+var getDemoSightings = function(){
+ $.ajax({
+    url:"/demo/sightings",
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){ 
+  console.log(response) 
+    var source = $("#demo-sighting-info").html();
+    var template = Handlebars.compile(source);
+    response.forEach(function(sighting){
+      $('body').append(template(sighting));
+    });
+  });
+};
+
+$(document).ready(function(){
+
+  getSightings();
+
+  $('#view-all-birds-btn').on('click', getDemoSightings)
+
+});
